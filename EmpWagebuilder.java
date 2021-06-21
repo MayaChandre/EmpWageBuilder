0a744b189835b45545c0bacbece4390e2ad373e7
mayavathi <mayachandre27@gmail.com>
2021-06-21 06:32:45 -0400

    Daily Employee Wage

--- EmpWagebuilder.java
+++ EmpWagebuilder.java
@@ -3,14 +3,18 @@ package com.bridgelabz.empwagebuilder;
 public class "Welcome to Employee Wage Computation Program"
 {
 	public static void main(String[] args) {
-		//constants
-		int IS_FULL_TIME = 1;
-		  //computation
+	        int FULLDAY = 8;
+		int WAGE_PER_HOUR = 20;
+		double IS_EMP_PRESENT = 1.0;
 		double empCheck = Math.floor(Math.random() * 10) % 2 ;
-        if (empCheck == IS_FULL_TIME)
+         if (empCheck == IS_EMP_PRESENT) {
+        	int dailywage = FULLDAY *WAGE_PER_HOUR;
        System.out.println("Employee is Present");
+        	System.out.println("daily Employee Wage is : "+ dailywage);
+        }
        else
     	   System.out.println("Employee is Absent");
-	
+	}
+
 	}
 }
